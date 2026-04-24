34b9563a166d3e1c40891199f09fa0dbf1c81734
mohith1402 <ballolamohith1402@gmail.com>
2026-04-24 16:16:53 +0530

    UC7: Addition with Target Unit Specification

--- QuantityMeasurementApp/src/com/apps/quantitymeasurement/QuantityMeasurementAppTest.java
+++ QuantityMeasurementApp/src/com/apps/quantitymeasurement/QuantityMeasurementAppTest.java
@@ -10,13 +10,67 @@ import com.apps.quantitymeasurement.QuantityMeasurementApp.Length.LengthUnit;
 
 public class QuantityMeasurementAppTest {
 
-    private static final double EPSILON = 1e-6;
+    private static final double EPSILON = 1e-2;
+
+    @Test
+    public void testAddition_WithTargetUnit_FeetAndInchesToYards() {
+        Length foot = new Length(1.0, LengthUnit.FEET);
+        Length inches = new Length(12.0, LengthUnit.INCHES);
+        Length result = foot.add(inches, LengthUnit.YARDS);
+
+        assertEquals(0.666, result.getValue(), EPSILON);
+        assertEquals(LengthUnit.YARDS, result.getUnit());
+    }
+
+    @Test
+    public void testAddition_WithTargetUnit_InchesAndCentimetersToInches() {
+        Length inches = new Length(2.0, LengthUnit.INCHES);
+        Length cm = new Length(2.54, LengthUnit.CENTIMETERS);
+        Length result = inches.add(cm, LengthUnit.INCHES);
+
+        assertEquals(3.0, result.getValue(), EPSILON);
+        assertEquals(LengthUnit.INCHES, result.getUnit());
+    }
+
+    @Test
+    public void testAddition_WithTargetUnit_YardsAndFeetToFeet() {
+        Length yard = new Length(1.0, LengthUnit.YARDS);
+        Length feet = new Length(2.0, LengthUnit.FEET);
+        Length result = yard.add(feet, LengthUnit.FEET);
+
+        assertEquals(5.0, result.getValue(), EPSILON);
+        assertEquals(LengthUnit.FEET, result.getUnit());
+    }
+
+    @Test
+    public void testAddition_WithTargetUnit_YardsAndYardsToCentimeters() {
+        Length yard1 = new Length(1.0, LengthUnit.YARDS);
+        Length yard2 = new Length(1.0, LengthUnit.YARDS);
+        Length result = yard1.add(yard2, LengthUnit.CENTIMETERS);
+
+        assertEquals(182.88, result.getValue(), EPSILON);
+        assertEquals(LengthUnit.CENTIMETERS, result.getUnit());
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testAddition_WithTargetUnit_NullLength_Throws() {
+        Length length = new Length(2.0, LengthUnit.INCHES);
+        length.add(null, LengthUnit.FEET);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testAddition_WithTargetUnit_NullTargetUnit_Throws() {
+        Length length1 = new Length(2.0, LengthUnit.INCHES);
+        Length length2 = new Length(2.0, LengthUnit.INCHES);
+        length1.add(length2, null);
+    }
 
     @Test
     public void testAddition_InchesToInches() {
         Length length1 = new Length(2.0, LengthUnit.INCHES);
         Length length2 = new Length(2.0, LengthUnit.INCHES);
         Length result = length1.add(length2);
+
         assertEquals(4.0, result.getValue(), EPSILON);
         assertEquals(LengthUnit.INCHES, result.getUnit());
     }
@@ -26,41 +80,9 @@ public class QuantityMeasurementAppTest {
         Length length1 = new Length(1.0, LengthUnit.FEET);
         Length length2 = new Length(2.0, LengthUnit.INCHES);
         Length result = length1.add(length2);
-        assertEquals(1.166666, result.getValue(), 0.01);
+
+        assertEquals(1.166666, result.getValue(), EPSILON);
         assertEquals(LengthUnit.FEET, result.getUnit());
-    }
-
-    @Test
-    public void testAddition_InchesToFeet() {
-        Length length1 = new Length(2.0, LengthUnit.INCHES);
-        Length length2 = new Length(1.0, LengthUnit.FEET);
-        Length result = length1.add(length2);
-        assertEquals(14.0, result.getValue(), EPSILON);
-        assertEquals(LengthUnit.INCHES, result.getUnit());
-    }
-
-    @Test
-    public void testAddition_FeetToFeet() {
-        Length length1 = new Length(1.0, LengthUnit.FEET);
-        Length length2 = new Length(1.0, LengthUnit.FEET);
-        Length result = length1.add(length2);
-        assertEquals(2.0, result.getValue(), EPSILON);
-        assertEquals(LengthUnit.FEET, result.getUnit());
-    }
-
-    @Test
-    public void testAddition_CentimetersToInches() {
-        Length length1 = new Length(2.0, LengthUnit.INCHES);
-        Length length2 = new Length(2.54, LengthUnit.CENTIMETERS);
-        Length result = length1.add(length2);
-        assertEquals(3.0, result.getValue(), 0.01);
-        assertEquals(LengthUnit.INCHES, result.getUnit());
-    }
-
-    @Test(expected = IllegalArgumentException.class)
-    public void testAddition_NullLength_Throws() {
-        Length length1 = new Length(2.0, LengthUnit.INCHES);
-        length1.add(null);
     }
 
     @Test
@@ -71,31 +93,10 @@ public class QuantityMeasurementAppTest {
     }
 
     @Test
-    public void testInchesEquality() {
-        Length inches1 = new Length(5.0, LengthUnit.INCHES);
-        Length inches2 = new Length(5.0, LengthUnit.INCHES);
-        assertTrue(inches1.equals(inches2));
-    }
-
-    @Test
     public void testFeetInchesComparison() {
         Length feet = new Length(1.0, LengthUnit.FEET);
         Length inches = new Length(12.0, LengthUnit.INCHES);
         assertTrue(feet.equals(inches));
-    }
-
-    @Test
-    public void testFeetInequality() {
-        Length feet1 = new Length(5.0, LengthUnit.FEET);
-        Length feet2 = new Length(10.0, LengthUnit.FEET);
-        assertFalse(feet1.equals(feet2));
-    }
-
-    @Test
-    public void testInchesInequality() {
-        Length inches1 = new Length(5.0, LengthUnit.INCHES);
-        Length inches2 = new Length(10.0, LengthUnit.INCHES);
-        assertFalse(inches1.equals(inches2));
     }
 
     @Test
