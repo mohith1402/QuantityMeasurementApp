6490a236e32b13e7cb79152234197343081da117
mohith1402 <ballolamohith1402@gmail.com>
2026-04-24 15:31:47 +0530

    UC2: Feet and Inches measurement equality

--- QuantityMeasurementApp/src/com/apps/quantitymeasurement/QuantityMeasurementApp.java
+++ QuantityMeasurementApp/src/com/apps/quantitymeasurement/QuantityMeasurementApp.java
@@ -1,8 +1,8 @@
 /*
- * UC1: Feet measurement equality
+ * UC2: Feet and Inches measurement equality
  *
  * @author Mohith
- * @version 1.0
+ * @version 2.0
  */
 
 package com.apps.quantitymeasurement;
@@ -29,14 +29,43 @@ public class QuantityMeasurementApp {
         }
     }
 
+    public static class Inches {
+        private final double value;
+
+        public Inches(double value) {
+            this.value = value;
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            if (this == obj) {
+                return true;
+            }
+            if (obj == null || getClass() != obj.getClass()) {
+                return false;
+            }
+            Inches inches = (Inches) obj;
+            return Double.compare(inches.value, this.value) == 0;
+        }
+    }
+
+    public static boolean compareFeet(double val1, double val2) {
+        Feet feet1 = new Feet(val1);
+        Feet feet2 = new Feet(val2);
+        return feet1.equals(feet2);
+    }
+
+    public static boolean compareInches(double val1, double val2) {
+        Inches inches1 = new Inches(val1);
+        Inches inches2 = new Inches(val2);
+        return inches1.equals(inches2);
+    }
+
     public static void main(String[] args) {
-        System.out.println("UC1: Feet measurement equality\n");
+        System.out.println("Feet Equality (10.0, 10.0): " + compareFeet(10.0, 10.0));
+        System.out.println("Feet Equality (10.0, 12.0): " + compareFeet(10.0, 12.0));
 
-        Feet feet1 = new Feet(10.0);
-        Feet feet2 = new Feet(10.0);
-        Feet feet3 = new Feet(12.0);
-
-        System.out.println("feet1 (10.0) equals feet2 (10.0): " + feet1.equals(feet2));
-        System.out.println("feet1 (10.0) equals feet3 (12.0): " + feet1.equals(feet3));
+        System.out.println("Inches Equality (5.0, 5.0): " + compareInches(5.0, 5.0));
+        System.out.println("Inches Equality (5.0, 7.0): " + compareInches(5.0, 7.0));
     }
 }
